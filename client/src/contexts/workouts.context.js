8d45b0c4d3e73de8c3cf2194d5dad7e10895e3ee
OtsoLap <otsolappalainen@gmail.com>
2022-03-09 19:31:32 +0200

    Put API calls on their own file. Cleaned up code.

--- client/src/contexts/workouts.context.js
+++ client/src/contexts/workouts.context.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext } from 'react'
 import workoutReducer from '../reducer/workout.reducer'
 import useLocalStorageReducer from '../hooks/useLocalStorageReducer'
-import axios from 'axios'
+import { getWorkouts, getWorkoutExercises, editExercise, postNewExercise, postNewWorkout } from '../util/apiCalls'
 
 const defaultWorkouts = [{ _id: "1", workoutDay: "Monday", workoutType: "Legs", workoutDone: false }]
 const defaultExercises = [{ workoutId: "1", _id: 11, move: "Pushups", reps: "3x10-15", link: "https://www.youtube.com/watch?v=Ee1YkE1jlyo", image: "https://i.ytimg.com/vi/Ee1YkE1jlyo/hqdefault.jpg", completed: false, }]
@@ -19,28 +19,6 @@ export function useDispatch() {
 export function WorkoutsProvider({ children }) {
     const [workouts, dispatchWorkout] = useLocalStorageReducer("workouts", defaultWorkouts, workoutReducer)
     const [exercises, dispatchExercise] = useLocalStorageReducer("exercises", defaultExercises, workoutReducer)
-
-    function getWorkouts() {
-        return axios.get('api/workouts')
-    }
-
-    function postNewWorkout(workout) {
-        return axios.post('api/workouts', workout)
-    }
-
-    function postNewExercise(exercise) {
-        return axios.post('api/exercises', exercise)
-    }
-
-    function editExercise(id, exercise) {
-        return axios.put(`api/exercises/${id}`, exercise)
-    }
-
-
-    function getWorkoutExercises(WorkoutId) {
-        return axios.get(`api/workouts/exercises/${WorkoutId}`)
-    }
-
 
 
     return (
